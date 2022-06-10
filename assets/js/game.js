aa0e52720f8a8ae59133874418b592568eaeb887
ocean <osciris.veliz@gmail.com>
2022-06-09 19:49:27 -0500

    integer input for shop prompt

--- assets/js/game.js
+++ assets/js/game.js
@@ -94,7 +94,7 @@ var fightOrSkip = function() {
             //return true if player wants to leave
             return true;
         } 
-        else (confirmSkip === false); {
+        else {
             return false;
         }
     } 
@@ -153,23 +153,21 @@ var fight = function(enemy) {
 var shop = function() {
     //ask player what they'd like to do
     var shopOptionPrompt = window.prompt(
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
         );
+        shopOptionPrompt = parseInt(shopOptionPrompt);
 
         // use switch to carry out action
         switch (shopOptionPrompt) {
-            case "REFILL": // new case
-            case "refill":
+            case 1:
                 playerInfo.refillHealth();
                 break;
 
-            case "UPGRADE":
-            case "upgrade":
+            case 2:
                 playerInfo.upgradeAttack();
                 break;
 
-            case "LEAVE":
-            case "leave":
+            case 3:
                 window.alert("Leave the store.");
                 // do nothing, so function will end
                 break;
